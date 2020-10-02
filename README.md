a2b436877a0413013e8737a18af24543ac699a24
martaluiz <martaluizavelino@gmail.com>
2020-10-02 10:23:49 -0700

    included some comments and finish the readme file

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Test-Alphabetical-Sentences
+
+The assignment is to arrange the sentences within the story in alphabetical order.
+
+TechStack: Javascript and Nodejs.
+
+## Project Setup
+
+1. Clone your own copy of this repo https://github.com/martaluiz/Test-Alphabetical-Sentences.
+2. Open the prpject folder on your computer using a code editor.
+3. Open  the terminal and install dependencies: $`npm install`
+4. Run the server: $`node app.js`
+5. The sentences will print in your terminal alphabetical order.
+
--- app.js
+++ app.js
@@ -1,23 +1,26 @@
+// Use fs.readFile() method to read the physical file ShortStory.txt.
 const fs = require('fs')
 
 fs.readFile('ShortStory.txt', (err, data) => {
     if (err) throw err;
     
+    //created a variable to indicate when the sentence is ended.
     let endSentenceIndicator = "*endSentenceIndicator*";
-
+    // use replace method to "replace" all new line character in your string with "endSentenceIndicator" 
     let str = data.toString().replace(/\n/g, endSentenceIndicator);
     str = str.replace(/\. /g, "."+ endSentenceIndicator);
 
+    // Use The split() method to split a string into an array of substrings, and returns the new array. 
     let array = str.split(endSentenceIndicator);
     let filteredArray = array.filter(function (element) {
       return element.match(/[A-Z]/g);
     });
 
-
+    // Use The sort() method to sorts the elements of an array in place and returns the alphabetical sorted array. 
     let sortedArray = filteredArray.sort();
     for (const element of sortedArray) {
         console.log(element);
-        console.log('\x1b[33m%s\x1b[0m',"--------------******************Separator")
+        console.log('\x1b[33m%s\x1b[0m',"--------------******************Separator--------------******************")
     }
 })
 
